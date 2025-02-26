79826c95c0f8073117cb0e7a62434d12837837c3
MunibaRahman <munibar@nycstudents.net>
2025-02-25 20:25:47 -0500

    Update grass.js

--- project/grass.js
+++ project/grass.js
@@ -3,108 +3,14 @@ class Grass{
 		this.x = x;
 		this.z = z;
 		
-		this.obj = document.createElement("a-entity")
-
-	 for(let i = 0; i < 10; i++){
 		let r = rnd(0.8,1.8);
-		let grass = document.createElement("a-gltf-model");
-		grass.setAttribute("src", "#grass")
-		grass.setAttribute("scale", `1 ${r} 1`);
-		grass.setAttribute("position",{x:-10 + 1 * i,y:0,z:0});
+		this.obj = document.createElement("a-gltf-model");
+		this.obj.setAttribute("src", "#grass")
+		this.obj.setAttribute("scale", `1 ${r} 1`);
+		this.obj.setAttribute("position",{x:x ,y:0,z:z});
 		
-		this.obj.append(grass);
-	 }
-	 for(let i = 0; i < 10; i++){
-		let r = rnd(0.8,1.8);
-		grass = document.createElement("a-gltf-model");
-		grass.setAttribute("src", "#grass")
-		grass.setAttribute("scale", `1 ${r} 1`);
-		grass.setAttribute("position",{x:-10 + 1 * i,y:0,z:1});
-
-		
-		this.obj.append(grass);
-	 }
-	 	 for(let i = 0; i < 10; i++){
-		let r = rnd(0.8,1.8);
-		grass = document.createElement("a-gltf-model");
-		grass.setAttribute("src", "#grass")
-		grass.setAttribute("scale", `1 ${r} 1`);
-		grass.setAttribute("position",{x:-10 + 1 * i,y:0,z:2});
-
-		
-		this.obj.append(grass);
-	 }
-	 	 for(let i = 0; i < 10; i++){
-		let r = rnd(0.8,1.8);
-		grass = document.createElement("a-gltf-model");
-		grass.setAttribute("src", "#grass")
-		grass.setAttribute("scale", `1 ${r} 1`);
-		grass.setAttribute("position",{x:-10 + 1 * i,y:0,z:3});
-
-		
-		this.obj.append(grass);
-	 }
-	 	 for(let i = 0; i < 10; i++){
-		let r = rnd(0.8,1.8);
-		grass = document.createElement("a-gltf-model");
-		grass.setAttribute("src", "#grass")
-		grass.setAttribute("scale", `1 ${r} 1`);
-		grass.setAttribute("position",{x:-10 + 1 * i,y:0,z:4});
-
-		
-		this.obj.append(grass);
-	 }
-	 	 for(let i = 0; i < 10; i++){
-		let r = rnd(0.8,1.8);
-		grass = document.createElement("a-gltf-model");
-		grass.setAttribute("src", "#grass")
-		grass.setAttribute("scale", `1 ${r} 1`);
-		grass.setAttribute("position",{x:-10 + 1 * i,y:0,z:5});
-
-		
-		this.obj.append(grass);
-	 }
-	 	 for(let i = 0; i < 10; i++){
-		let r = rnd(0.8,1.8);
-		grass = document.createElement("a-gltf-model");
-		grass.setAttribute("src", "#grass")
-		grass.setAttribute("scale", `1 ${r} 1`);
-		grass.setAttribute("position",{x:-10 + 1 * i,y:0,z:6});
-
-		
-		this.obj.append(grass);
-	 }
-	 	 for(let i = 0; i < 10; i++){
-		let r = rnd(0.8,1.8);
-		grass = document.createElement("a-gltf-model");
-		grass.setAttribute("src", "#grass")
-		grass.setAttribute("scale", `1 ${r} 1`);
-		grass.setAttribute("position",{x:-10 + 1 * i,y:0,z:7});
-
-		
-		this.obj.append(grass);
-	 }
-	 	 for(let i = 0; i < 10; i++){
-		let r = rnd(0.8,1.8);
-		grass = document.createElement("a-gltf-model");
-		grass.setAttribute("src", "#grass")
-		grass.setAttribute("scale", `1 ${r} 1`);
-		grass.setAttribute("position",{x:-10 + 1 * i,y:0,z:8});
-
-		
-		this.obj.append(grass);
-	 }
-	 	 for(let i = 0; i < 10; i++){
-		let r = rnd(0.8,1.8);
-		grass = document.createElement("a-gltf-model");
-		grass.setAttribute("src", "#grass")
-		grass.setAttribute("scale", `1 ${r} 1`);
-		grass.setAttribute("position",{x:-10 + 1 * i,y:0,z:9});
-
-		
-		this.obj.append(grass);
-	 }    
 		scene.append(this.obj);
-	}
-}
+	 }
 
+		
+	}
